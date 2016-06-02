b3a0a4104aa255a46ef66a380ff8fe6bca490c2c
Srujan Akmarthi <acsrujan@gmail.com>
2016-06-02 12:21:46 +0530

    Permissions and shit

--- run.sh
+++ run.sh
@@ -3,11 +3,14 @@
 /bin/bash nodejs_install.sh
 sudo touch /etc/nginx/upstream.conf
 mkdir -p /var/www/
+chmod 755 /var/www/
+chmod 644 /var/www/*
 cd /var/www/
 git clone git@github.com:chetandhembre/hello-world-node.git
 cd /var/www/hello-world-node/
 forever start main.js
 PORT=8081 forever start main.js
-chmod +x check_rpm.sh
-cp check_rpm.sh /etc/check_rpm
+chmod +x $PWD/check_rpm.sh
+cp $PWD/check_rpm.sh /etc/check_rpm
+cp $PWD/reqpmin.conf /etc/init/reqpmin.conf
 sudo service reqpmin start
