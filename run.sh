1c44ae1a508ff97265f60488bfb8689a566daa25
acsrujan <acsrujan@gmail.com>
2016-06-02 07:10:55 +0530

    master file to run all necessary setup

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo nginx_install.sh
+sudo nodejs_install.sh
+sudo touch /etc/nginx/upstream.conf
+mkdir -p /var/www/
+cd /var/www/
+git clone git@github.com:chetandhembre/hello-world-node.git
+cd /var/www/hello-world-node/
+forever start main.js
+PORT=8081 forever start main.js
+chmod +x check_rpm.sh
+cp check_rpm.sh /etc/check_rpm
+sudo service reqpmin start
