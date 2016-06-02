f9db43164c4cc9e9939b09c00733141c2285ca27
Srujan Akmarthi <acsrujan@gmail.com>
2016-06-02 12:32:35 +0530

    makes some permission and file path changes

--- run.sh
+++ run.sh
@@ -5,12 +5,13 @@ sudo touch /etc/nginx/upstream.conf
 mkdir -p /var/www/
 sudo chmod 755 /var/www/
 sudo chmod 644 /var/www/*
-cd /var/www/
+cd ~/
 git clone git@github.com:chetandhembre/hello-world-node.git
+sudo mv ~/hello-world-node /var/www/
 cd /var/www/hello-world-node/
-forever start main.js
-PORT=8081 forever start main.js
-sudo chmod +x /var/www/nodejs-nginx-load/check_rpm.sh
-sudo cp /var/www/nodejs-nginx-load/check_rpm.sh /etc/check_rpm
-sudo cp /var/www/nodejs-nginx-load/reqpmin.conf /etc/init/reqpmin.conf
+$HOME/local/node/bin/forever start main.js
+PORT=8081 $HOME/local/node/bin/forever start main.js
+sudo chmod +x $HOME/nodejs-nginx-load/check_rpm.sh
+sudo cp $HOME/nodejs-nginx-load/check_rpm.sh /etc/check_rpm
+sudo cp $HOME/nodejs-nginx-load/reqpmin.conf /etc/init/reqpmin.conf
 sudo service reqpmin start
