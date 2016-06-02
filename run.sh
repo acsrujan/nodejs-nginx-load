580c40bd3800af6e1ad53cee47014821210f3707
Srujan Akmarthi <acsrujan@gmail.com>
2016-06-02 14:06:51 +0530

    remove chmod, upload executable bit on git

--- run.sh
+++ run.sh
@@ -2,16 +2,9 @@
 /bin/bash nginx_install.sh
 /bin/bash nodejs_install.sh
 sudo touch /etc/nginx/upstream.conf
-mkdir -p /var/www/
-sudo chmod 755 /var/www/
-sudo chmod 644 /var/www/*
-cd ~/
+cd $HOME
 git clone git@github.com:chetandhembre/hello-world-node.git
-sudo mv ~/hello-world-node /var/www/
-cd /var/www/hello-world-node/
-$HOME/local/node/bin/forever start main.js
-PORT=8081 $HOME/local/node/bin/forever start main.js
-sudo chmod +x $HOME/nodejs-nginx-load/check_rpm.sh
-sudo cp $HOME/nodejs-nginx-load/check_rpm.sh /etc/check_rpm
+$HOME/local/node/bin/forever start $HOME/hello-world-node/main.js
+PORT=8081 $HOME/local/node/bin/forever start $HOME/hello-world-node/main.js
 sudo cp $HOME/nodejs-nginx-load/reqpmin.conf /etc/init/reqpmin.conf
 sudo service reqpmin start
