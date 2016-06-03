54b72df5c5d31a3b00b16eff51839371af3acfb4
Srujan Akmarthi <acsrujan@gmail.com>
2016-06-03 13:29:51 +0530

    use zip instead of ssh

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 /bin/bash nodejs_install.sh
 sudo touch /etc/nginx/upstream.conf
 cd $HOME
-git clone git@github.com:chetandhembre/hello-world-node.git
+wget https://github.com/chetandhembre/hello-world-node/archive/master.zip
+unzip *-master.zip -d hello-world-node
+rm -rf *-master.zip
 $HOME/local/node/bin/forever start $HOME/hello-world-node/main.js
 PORT=8081 $HOME/local/node/bin/forever start $HOME/hello-world-node/main.js
 sudo cp $HOME/nodejs-nginx-load/reqpmin.conf /etc/init/reqpmin.conf
