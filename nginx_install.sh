22bcd4a0425d7d33954516fcdb05d130584d7c2f
Srujan Akmarthi <acsrujan@gmail.com>
2016-06-03 13:30:25 +0530

    Install unzip if not there

--- nginx_install.sh
+++ nginx_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y openssh-server libssl-dev git g++ make python-software-properties
+sudo apt-get install -y openssh-server libssl-dev git g++ make python-software-properties unzip
 sudo apt-get install -y nginx nginx-extras
